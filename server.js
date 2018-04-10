5b215b56df310707126f2857b84a3e614b8bb958
Reena <reena.sanjay.sharma@gmail.com>
2018-04-10 23:45:35 +0100

    serve static files

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
