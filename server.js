bb73bdb0b96e1d388186259593757ecb8ba05811
Reena <reena.sanjay.sharma@gmail.com>
2018-04-12 12:22:48 +0100

    addesd const PORT in server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
